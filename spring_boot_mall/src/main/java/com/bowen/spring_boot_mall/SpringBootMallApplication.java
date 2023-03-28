c41ac2ad23a03847d3eae640c07b1bb644fdc256
Bowen <a0926108752@gmail.com>
2023-03-28 21:39:36 +0800

    初始化 spring boot mall project

---
+++ spring_boot_mall/src/main/java/com/bowen/spring_boot_mall/SpringBootMallApplication.java
@@ -0,0 +1,13 @@
+package com.bowen.spring_boot_mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMallApplication.class, args);
+	}
+
+}
---
+++ spring_boot_mall/src/test/java/com/bowen/spring_boot_mall/SpringBootMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bowen.spring_boot_mall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
